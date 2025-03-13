9fbe00e5d70e32d7e69cf3dc194a7f6b83b9cc8a
Lou <marilounacional0717@gmail.com>
2025-03-13 17:18:58 +0800

    bigger number is the first number

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,7 +1,8 @@
 # ask user for two numbers
 num1 = float(input("Enter your first number: "))
 num2 = float(input("Enter your second number: "))
-# if num1 is greater than num2
-    # print num1
+# if num1 is greater than num2 then print num1
+if num1 > num2:
+    print("The bigger number is", num1)
 # if num2 is greater than num1
     # print num2

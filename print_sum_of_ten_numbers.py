236b47bfe9a4a16d6744c3189729bd530611da7f
Lou <marilounacional0717@gmail.com>
2025-03-13 20:55:30 +0800

    initialize and calculate

--- print_sum_of_ten_numbers.py
+++ print_sum_of_ten_numbers.py
@@ -1,6 +1,8 @@
 # initialize the total
+total = 0
 # input ten numbers
 for i in range(10):
     num = int(input(f"Enter number {i +1}: "))
 # calculate the sum of all numbers
+total += num
 # print the sum

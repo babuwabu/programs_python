61bdcec0053c88158b2edf74322915b9980dcbab
Lou <marilounacional0717@gmail.com>
2025-03-13 17:23:16 +0800

    whole number and bigger number is the second number

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,8 +1,9 @@
 # ask user for two numbers
-num1 = float(input("Enter your first number: "))
-num2 = float(input("Enter your second number: "))
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 # if num1 is greater than num2 then print num1
 if num1 > num2:
     print("The bigger number is", num1)
-# if num2 is greater than num1
-    # print num2
+# if num2 is greater than num1 then print num2
+elif num2 > num1:
+    print("The bigger number is", num2)

8ed7a99e57a3feeabf0755fff880809c7cc7c1e8
Lou <marilounacional0717@gmail.com>
2025-03-13 17:14:13 +0800

    ask two inputs

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,4 +1,6 @@
 # ask user for two numbers
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
 # if num1 is greater than num2
     # print num1
 # if num2 is greater than num1

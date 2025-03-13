e993150588ed2a061c898b75d603e37f913a6fef
Lou <marilounacional0717@gmail.com>
2025-03-13 21:23:48 +0800

    count odds

--- print_how_many_odds.py
+++ print_how_many_odds.py
@@ -1,6 +1,9 @@
 # initialize
+odds = 0
 # input ten numbers
 for i in range(10):
-     num = int(input(f"Enter number {i +1}: "))
-# checking the number of odds
+    num = int(input(f"Enter number {i +1}: "))
+    if num % 2 != 0:
+        odds += 1
 # print how many odds
+print("The total of odd numbers is", odds)

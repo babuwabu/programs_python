55b8ff75f113f726de5a8323770b7735f62d9e30
Lou <marilounacional0717@gmail.com>
2025-03-13 20:39:15 +0800

    calculate

--- print_first_raised_second.py
+++ print_first_raised_second.py
@@ -2,4 +2,5 @@
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 # calculate the result of the first number raised to the second number
+result = num1 ** num2
 # print result

8f133c96449d0ab113ce252ef25a4b6d8e7d1273
Lou <marilounacional0717@gmail.com>
2025-03-14 11:15:09 +0800

    ask for two inputs

--- print_difference_of_two_numbers.py
+++ print_difference_of_two_numbers.py
@@ -1,3 +1,5 @@
 # ask for two inputs
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 # calculate the difference of two numbers
 # print the difference

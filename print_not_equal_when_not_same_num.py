1737ac0f24852b18a7e5beac0660003a617dde68
Lou <marilounacional0717@gmail.com>
2025-03-14 11:04:52 +0800

    pseudocode

--- print_not_equal_when_not_same_num.py
+++ print_not_equal_when_not_same_num.py
@@ -1,3 +1,5 @@
 # ask for two inputs
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 # check if the numbers are not the same
 # if true print not equal

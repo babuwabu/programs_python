b23787c61ed83a09f8c1e5bc101ef2faa2955cf2
Lou <marilounacional0717@gmail.com>
2025-03-14 19:50:18 +0800

    pseudocode

---
+++ print_unique_duplicate.py
@@ -0,0 +1,6 @@
+# ask user inputs and loop for the user to input continously until invalid
+# checks number if duplicate
+# if true print duplicate
+# checks number if unique
+# if true print unique
+# stop the input when invalid

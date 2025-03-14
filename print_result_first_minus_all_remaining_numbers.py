683598fed895b65e662972848c7b855feb3baef7
Lou <marilounacional0717@gmail.com>
2025-03-14 15:15:07 +0800

    ask ten inputs from user

--- print_result_first_minus_all_remaining_numbers.py
+++ print_result_first_minus_all_remaining_numbers.py
@@ -1,3 +1,4 @@
 # ask to input 10 numbers
+nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
 # calculate the result
 # print the result of the first number minus all the remaining numbers

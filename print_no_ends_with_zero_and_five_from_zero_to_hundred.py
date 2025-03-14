a2cc600d569bed25ad6e0a6afeba37fbb2fb2ab4
Lou <marilounacional0717@gmail.com>
2025-03-14 18:11:57 +0800

    print the numbers without zeroes and fives

--- print_no_ends_with_zero_and_five_from_zero_to_hundred.py
+++ print_no_ends_with_zero_and_five_from_zero_to_hundred.py
@@ -1,3 +1,5 @@
 # outputs numbers zero to one hundred
-# check if numbers end with zero and five
-# print the numbers 
+for num in range(101):
+    if num % 10 != 0 and num % 10 != 5: # check if numbers end with zero and five
+        print(num) # print the numbers 
+

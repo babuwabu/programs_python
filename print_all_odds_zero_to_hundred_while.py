61cbf1beb13ce59fb0c0e55f28928227893a9c38
Lou <marilounacional0717@gmail.com>
2025-03-14 18:00:09 +0800

    print all odd numbers with while loop

--- print_all_odds_zero_to_hundred_while.py
+++ print_all_odds_zero_to_hundred_while.py
@@ -1,3 +1,6 @@
 # initialize num
-# while loop and check all the odds
-# print the odd numbers from zero to one hundred
+num = 1
+# while loop and check all the odds then print the odd numbers from zero to one hundred
+while num <= 100:
+    print(num)
+    num += 2

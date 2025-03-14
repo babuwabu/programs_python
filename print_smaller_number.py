e588a7359d8fdc999342737209a32a9b1cae9bfc
Lou <marilounacional0717@gmail.com>
2025-03-14 10:56:46 +0800

    if num1 is less than num2 then print num1

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -2,4 +2,6 @@
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 # if num1 is less than num2 then print num1
+if num1 < num2:
+    print("The smaller number is", num1)
 # if num2 is less than num1 then print num2

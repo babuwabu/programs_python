95122915d8052836dd639027401767849e23f5f8
Lou <marilounacional0717@gmail.com>
2025-03-14 14:53:51 +0800

    print the remainder of the first number divided by the second number

--- print_remainder_first_divide_second.py
+++ print_remainder_first_divide_second.py
@@ -4,3 +4,4 @@ num2 = float(input("Enter your second number: "))
 # calculate and get the remainder
 remainder = num1 % num2
 # print the remainder when the first number is divided by the second number
+print("The remainder of the first number divided by the second number is", remainder)

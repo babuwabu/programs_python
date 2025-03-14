737501ecc25c189bd16e84a48fd39c2bd6461feb
Lou <marilounacional0717@gmail.com>
2025-03-14 11:55:49 +0800

    calculate diff

--- print_difference_of_two_numbers.py
+++ print_difference_of_two_numbers.py
@@ -2,4 +2,5 @@
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 # calculate the difference of two numbers
+diff = num1 - num2
 # print the difference

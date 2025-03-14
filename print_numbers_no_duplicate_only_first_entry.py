b20da0fc611c03a47a67b11add937c4edc538afb
Lou <marilounacional0717@gmail.com>
2025-03-14 19:01:59 +0800

    print all numbers if duplicate keep first entry

--- print_numbers_no_duplicate_only_first_entry.py
+++ print_numbers_no_duplicate_only_first_entry.py
@@ -1,4 +1,8 @@
 # ask for ten numbers
 nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
+seen = set() # keeps the numbers that could be duplicated
 # checks the number if it's duplicate and keep the first entry
-# print those numbers
+for num in nums:
+    if num not in seen:
+        print(num) # print those numbers
+        seen.add(num) # mark numbers seen

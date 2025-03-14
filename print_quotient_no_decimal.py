3a7dbeb179d322887e3abc294e4a848705ada372
Lou <marilounacional0717@gmail.com>
2025-03-14 14:37:49 +0800

    ask two nums

--- print_quotient_no_decimal.py
+++ print_quotient_no_decimal.py
@@ -1,3 +1,5 @@
 # ask for two numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 # calculate the quotient without the decimal point
 # print the quotient of the two numbers without the decimal point

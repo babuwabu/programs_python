4c84f76b6325a7098bc00b1a86634b26712d5fe0
Lou <marilounacional0717@gmail.com>
2025-03-14 17:24:27 +0800

    initialize and change the asking of inputs

--- print_how_many_even_numbers.py
+++ print_how_many_even_numbers.py
@@ -1,4 +1,7 @@
+# initialize number of even numbers
+evens = 0
 # ask to input 10 numbers
-nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
 # calculate how many are the even numbers
 # print the number of evens

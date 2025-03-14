7bd7eed7b4390dfeacc303eb701e0911106576ca
Lou <marilounacional0717@gmail.com>
2025-03-14 17:27:40 +0800

    checks how many the even numbers

--- print_how_many_even_numbers.py
+++ print_how_many_even_numbers.py
@@ -1,7 +1,8 @@
 # initialize number of even numbers
 evens = 0
-# ask to input 10 numbers
+# ask to input 10 numbers and calculate how many are the even numbers
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
-# calculate how many are the even numbers
+    if num % 2 == 0:
+        evens += 1
 # print the number of evens

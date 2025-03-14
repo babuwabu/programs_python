5756b617b4ae9181041ee8298f001dbf7b5277ba
Lou <marilounacional0717@gmail.com>
2025-03-14 14:49:11 +0800

    ask two numbers

--- print_remainder_first_divide_second.py
+++ print_remainder_first_divide_second.py
@@ -1,3 +1,5 @@
 # ask for two numbers to input
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
 # calculate and get the remainder
 # print the remainder when the first number is divided by the second number

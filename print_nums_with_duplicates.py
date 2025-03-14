4d951f204ad6b483327863fec0239332dbb99099
Lou <marilounacional0717@gmail.com>
2025-03-14 22:12:02 +0800

    print the numbers that has a duplicate

--- print_nums_with_duplicates.py
+++ print_nums_with_duplicates.py
@@ -1,3 +1,8 @@
 # ask ten numbers
+nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
 # check numbers and if the number is duplicated
-# print the duplicated numbers
+seen = set()
+for num in nums:
+    if nums.count(num) > 1 and num not in seen:
+        print(num) # print the duplicated numbers
+        seen.add(num)

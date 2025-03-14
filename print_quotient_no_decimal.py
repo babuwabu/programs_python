53561b1bee4dd96044b4a5fd2ae73379352b6922
Lou <marilounacional0717@gmail.com>
2025-03-14 14:41:01 +0800

    print quotient with no decimal point

--- print_quotient_no_decimal.py
+++ print_quotient_no_decimal.py
@@ -4,3 +4,4 @@ num2 = int(input("Enter your second number: "))
 # calculate the quotient without the decimal point
 quotient = num1 // num2
 # print the quotient of the two numbers without the decimal point
+print("The quotient is", quotient)

d9c6d7eb765059037227678e852149533936a959
Lou <marilounacional0717@gmail.com>
2025-03-14 18:45:38 +0800

    print the numbers that has no duplicates

--- print_numbers_no_duplicates.py
+++ print_numbers_no_duplicates.py
@@ -1,4 +1,6 @@
 # ask for ten numbers
 nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
 # checking if the numbers are duplicated
-# print the numbers
+for num in nums:
+    if nums.count(num) == 1:
+        print(num) # print the numbers that has no duplicate

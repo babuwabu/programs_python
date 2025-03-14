feaaf4cbb80973e3f96ae40e0ccc586d22be8f10
Lou <marilounacional0717@gmail.com>
2025-03-14 10:54:15 +0800

    ask two numbers

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,3 +1,5 @@
 # ask for two numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
 # if num1 is less than num2 then print num1
 # if num2 is less than num1 then print num2

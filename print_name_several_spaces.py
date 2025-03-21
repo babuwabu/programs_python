4d4047490a84d61c69fe7bb679b1015a7440edfd
Marilou Nacional <marilounacional0717@gmail.com>
2025-03-22 07:28:38 +0800

    Delete print_name_several_spaces.py

--- print_name_several_spaces.py
+++
@@ -1,3 +0,0 @@
-# ask fullname of the user and make several spaces at the beginning
-fullname = input("Enter your fullname with spaces at the beginning: ").lstrip()
-print(fullname) # print the fullname

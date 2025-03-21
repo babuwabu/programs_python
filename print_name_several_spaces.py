ee878ef08a529263e3d25984788553b3d1d9bcdc
Lou <marilounacional0717@gmail.com>
2025-03-21 10:54:53 +0800

    changed

---
+++ print_name_several_spaces.py
@@ -0,0 +1,3 @@
+# ask fullname of the user and make several spaces at the beginning
+fullname = input("Enter your fullname with spaces at the beginning: ").lstrip()
+print(fullname) # print the fullname

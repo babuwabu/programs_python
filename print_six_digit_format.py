d2d93c4625521164a734cfa006b7ab2521da58c2
Marilou Nacional <marilounacional0717@gmail.com>
2025-03-22 07:28:50 +0800

    Delete print_six_digit_format.py

--- print_six_digit_format.py
+++
@@ -1,5 +0,0 @@
-# ask numbers from (0-1000)
-number = int(input("Enter number from (0-1000): "))
-# add zeros at the beginning to complete the six digit
-# print it on six digit format
-print(f"{number:06}")
